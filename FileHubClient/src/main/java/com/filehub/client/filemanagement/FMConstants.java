670fc035bd1c8adbc2c54a8d94d374829556fdcf
Abi <abinandhan952@gmail.com>
2025-10-17 21:56:38 +0530

    Added download file & upload file feature

---
+++ FileHubClient/src/main/java/com/filehub/client/filemanagement/FMConstants.java
@@ -0,0 +1,8 @@
+package com.filehub.client.filemanagement;
+
+public class FMConstants {
+    public static final String SERVER_IP = "http://10.250.125.119";
+    public static final String SERVER_URL = "http://10.250.125.119:8081";
+    public static final String SERVER_PORT = "8081";
+    public static final String DOWNLOAD_PATH = "E:\\Downloads\\";
+}
